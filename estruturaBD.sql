b645ce00fbe7a30e942e077966161819f78d210e
cffdo20 <cffdo2.0@gmail.com>
2024-02-23 20:48:25 -0400

    Atualização do DER e do BD

--- estruturaBD.sql
+++ estruturaBD.sql
@@ -24,9 +24,46 @@ create table voluntario(
     volnomesocial varchar(100) null,
     volbio varchar(300),
     voltelefone char(11) not null,
+    volcidcod int unsigned not null,
     primary key (volcpf),
-    foreign key (voluscod) references usuario(uscodigo)
+    foreign key (voluscod) references usuario(uscodigo),
+    foreign key (volcidcod) references cidade(cidcodigo)
 );
 
-create table 
+create table projetosocial(
+	projid int unsigned not null auto_increment,
+    projtitulo varchar(100) not null,
+    projdescricao varchar(500) not null,
+    projpublicoalvo varchar(45) not null,
+    projjustificativa varchar(100) not null,
+    projobjetivos varchar(100) not null,
+    projdatainicio date not null,
+    projdatafinal date null,
+    projstatus tinyint not null,
+    projuscodigo int not null,
+    primary key(projid),
+    foreign key (projuscodigo) references usuario (uscodigo)
+);
 
+create table atividade();
+
+create table cidade(
+	cidcodigo int unsigned not null auto_increment,
+    
+);
+
+create table estado();
+
+create table habilidade();
+
+create table voluntariohabilidade();
+
+create table diasemana();
+
+create table turnodia();
+
+create table voluntariodiasemana();
+
+create table voluntarioprojeto();
+
+create table instituicaoprojeto();
