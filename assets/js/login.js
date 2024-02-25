7f263b1244abd8512195bb831766bbcc0f1cb375
george-valente <valentegeorge71@gmail.com>
2024-02-25 18:41:16 -0400

    Página de Cadastro 2/3

---
+++ assets/js/login.js
@@ -0,0 +1,17 @@
+/* Visualizar senha */
+
+const passwordInput = document.querySelector("#login-senha"); 
+const passwordBtn = document.querySelector("#password-btn"); 
+
+passwordBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if(passwordInput.type === "password"){
+        passwordInput.type = "text"
+    }
+    else{
+        passwordInput.type = "password"; 
+    }
+})
+
+
