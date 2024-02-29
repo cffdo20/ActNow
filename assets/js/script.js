3863c6c0ceb659fa2e0067276f7529eb872367cf
george-valente <valentegeorge71@gmail.com>
2024-02-29 14:47:24 -0400

    Início View Cadastro Atividade

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,10 @@
 const btnTema = document.querySelector("#btn-tema"); 
 const body = document.body; 
 
-
+/* var typed = new Typed(".main-name", {
+    strings: ["Conheça o ActNow", "Meet ActNow"], 
+    typeSpeed: 150, 
+    backSpeed: 150, 
+    loop: true
+}) */
 
