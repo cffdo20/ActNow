88db657f5c91db903b9aaaf4590f45b920b85184
JDDPacheco <josediogo.dp@gmail.com>
2024-03-03 19:54:05 -0400

    Alterada forma de receber a requisição HTTP e rota para resposta

--- actnow_backend.js
+++ actnow_backend.js
@@ -5,18 +5,18 @@ const UsuController = require('./controller/UsuController.js');
 // Importando 'express' para requisiçõe HTTP
 const express = require('express');
 const app = express();
-
-// Importando 'cors' para permitir requisições HTTP de outro local
-const cors = require('cors');
-
 // Setando a porta a ser usada no localhost
 const PORT = 3000;
 
+// Importando 'body-parser' para lidar com o corpo da requisição HTTP
+const bodyParser = require('body-parser');
+// Configuração do body-parser para lidar com o corpo das requisições
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/*/ Importando 'cors' para permitir requisições HTTP de outro local
+const cors = require('cors');
 // Adicionando middleware para permitir requisições HTTP de outro local
 app.use(cors());
-
-// Adicionadno middleware para lidar com o corpo de solicitações HTTP formatadas em JSON 
-app.use(express.json());
 
 /** Simulação de evento de usario criando um Projeto */
 // Recebemos uma instancia de usuário de exemplo
@@ -24,11 +24,10 @@ const usuario = UsuController.gerarUsuarioExemplo();
 
 // Rota para lidar com a solicitação POST enviada pelo formulário
 app.post('/projetos', (req, res) => {   // Recebemos os dados do projeto informados pelo Front-End
-    // Exibe os dados recebidos do frontend no console
-    console.log('Dados recebidos do frontend:', req.body);
 
     // A função que cria um projeto é chamada
-    let projetonovo = ProjController.criarProjeto(usuario ,req.body.idProjeto, req.body.tituloProj, req.body.descricaoProj, req.body.publicoAlvoProj, req.body.justificativaProj, req.body.objetivosProj, req.body.dataInicioProj, req.body.statusProj);
+    let projetonovo = ProjController.criarProjeto(usuario , 1, req.body.titulo, req.body.descricao, req.body.publicoAlvo,
+        req.body.justificativa, req.body.objetivos, req.body.dataInicio, req.body.status);
 
     //Gerar uma sáida HTML para visualização no localhost
     let htmlContent = `
@@ -40,26 +39,25 @@ app.post('/projetos', (req, res) => {   // Recebemos os dados do projeto informa
             <title>Página HTML Dinâmica</title>
         </head>
         <body>
-            <h1>Dados do Projeto</h1>
+            <h1>Dados do Projeto Criado</h1>
             <p>Título do Projeto: ${projetonovo.tituloProj}</p>
             <p>Descrição: ${projetonovo.descricaoProj}</p>
             <p>Público-Alvo: ${projetonovo.publicoAlvoProj}</p>
             <p>Justificativa: ${projetonovo.justificativaProj}</p>
             <p>Objetivo do Projeto: ${projetonovo.objetivosProj}</p>
             <p>Data de Início: ${projetonovo.dataInicioProj}</p>
-            <p>Username do Criador/Gestor: ${projetonovo.usuario.idUsuario}</p>
+            <p>id do Usuário Criador/Gestor: ${projetonovo.usuario.idUsuario}</p>
             <p>Username do Criador/Gestor: ${projetonovo.usuario.userName}</p>
         </body>
         </html>
         `;
 
     // Rota para enviar a resposta HTML
-    app.get('/', (req, res) => {
-    res.send(htmlContent);
-    });
+    //app.get('/projetos', (req, res) => {
+    //});
     
     // Responde ao frontend com uma mensagem de sucesso
-    res.json({ mensagem: 'Dados recebidos com sucesso, projeto criado!' });
+    res.send(htmlContent);
 
 });
 
