ca5aeb53a880771967a77b80a9ad192218c0496a
JDDPacheco <josediogo.dp@gmail.com>
2024-03-03 21:15:37 -0400

    Ajustada function de criação de projeto

--- controller/ProjController.js
+++ controller/ProjController.js
@@ -1,21 +1,24 @@
 // Importando a classe ProjetoSocial
 const ProjetoSocial = require('../model/ProjetoSocial.js');
 
-class ProjController {
-    criarProjeto(usuario, idProjeto, tituloProj, descricaoProj,
-        publicoAlvoProj, justificativaProj, objetivosProj,
-        dataInicioProj, statusProj) {
-        
-        // Converter a Data
-        this.dataInicioProj = new Date(dataInicioProj);
+function criarProjeto(usuario, tituloProj, descricaoProj,
+    publicoAlvoProj, justificativaProj, objetivosProj,
+    dataInicioProj, statusProj) {
 
-        var novoProjeto = new ProjetoSocial(idProjeto, tituloProj, descricaoProj, publicoAlvoProj,
-            justificativaProj, objetivosProj, this.dataInicioProj, statusProj);
-        
-        novoProjeto.adicionarGestorProj(usuario);
-        
-        return novoProjeto;
-    }
+    // Converter a Data
+    this.dataInicioProj = new Date(dataInicioProj);
+
+    // Aqui vem a função que buscará no banco de dados qual o próximo id disponível, por enquanto será setado sempre como '1'
+    let idProjeto = 1;
+
+    // Criação do novo projeto
+    var novoProjeto = new ProjetoSocial(idProjeto, tituloProj, descricaoProj, publicoAlvoProj,
+        justificativaProj, objetivosProj, this.dataInicioProj, statusProj);
+    
+    //Adicionar o usuário como gestor do projeto
+    novoProjeto.adicionarGestorProj(usuario);
+    
+    return novoProjeto;
 }
 
-module.exports = new ProjController();
+module.exports = {criarProjeto};
