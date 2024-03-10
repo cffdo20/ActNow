0be4ec39876a0a056905ab7eb93739651ca8253f
cffdo20 <cffdo2.0@gmail.com>
2024-03-10 19:38:23 -0400

    Preparando branch para merge da sprint 1

---
+++ db.js
@@ -0,0 +1,36 @@
+async function connect() {
+  if (global.connection && global.connection.state !== 'disconnected')
+    return global.connection;
+
+  const mysql = require("mysql2/promise");
+  const connection = await mysql.createConnection("mysql://root:02114002@localhost:3306/actnow");
+  console.log("Conectou no MySQL!");
+  global.connection = connection;
+  return connection;
+}
+async function callSelectProcedure(nomeProcedure) {
+  const conn = await connect();
+  const resposta = await conn.query('CALL ' + nomeProcedure + ';');
+  return resposta;
+}
+async function callProcedureWithParameter(nomeProcedure, parametros = []) {
+  const conn = await connect();
+  const sql = 'Call ' + nomeProcedure + '(?);';
+  const values = concatParametros(parametros);
+  const [resposta] = await conn.query(sql, values);
+  return resposta;
+}
+function concatParametros(parametros = []) {
+  var conpar = '';
+  var i = 0
+  do {
+    if (i == parametros.length - 1) {
+      conpar = conpar + String(parametros[i]) + ','
+    } else {
+      conpar = conpar + String(parametros[i]) + ','
+    }
+    i++
+  } while (i < parametros.length)
+  return conpar
+}
+module.exports = { callProcedureWithParameter }
