d353605b30e74250b133e4932d0af13bf1cf7d8b
JDDPacheco <josediogo.dp@gmail.com>
2024-03-10 19:29:48 -0400

    Adaptado com rotas e .ejs

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Importando módulos Controllers
 const projetoController = require('./controller/ProjController.js');
 
+// Importando 'ejs' para Engine View
+//const ejs = require('ejs');
 // Importando 'express' para requisiçõe HTTP e inicialização do host
+const path = require('path'); // Importando o módulo 'path'
 const express = require('express');
 // Importando 'body-parser' para lidar com o corpo das requisições HTTP
 const bodyParser = require('body-parser');
@@ -10,40 +13,30 @@ const app = express();
 // Configuração do EJS como view engine
 app.set('view engine', 'ejs');
 // Definição do diretório onde estão os arquivos de visualização (opcional)
-app.set('view', path.join(__dirname, 'view'));
+app.set('views', path.join(__dirname, 'ActNow-front-end'));
 // Configurando const app para 'body-parser' que lida com o conteúdo das requisições HTTP 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 /** Definição das Rotas */
-// Define a rota para o index
-app.get('/index', (req, res) => {
-    // Envie a página HTML preexistente
-    res.sendFile(__dirname + '/ActNow-front-end/index.html');
-});
-// Define a rota para a página do projeto criado
-app.get('/meu-projeto', (req, res) => {
-    // Envie a página HTML preexistente
-    //res.sendFile(__dirname + '/ActNow-front-end/visualizacao-projeto.html');
-    //Outra abordagem
-    // Os dados que você deseja passar para o template
-    const data = {
-        titulo: 'Projeto criado com sucesso!',
-        conteudo: 'Olá, mundo!'
-    };
-
-    // Renderiza a página 'pagina.ejs' e envia para o cliente
-    res.render('meu-projeto', data);
-});
-
-/** Acionamento dos controllers para cada rota acionada | Rotas para lidar com as solicitaçõs POST enviada pelo formulário(HTML)*/
 // Criação de novo projeto
 app.post('/criar-projeto', (req, res) => {
     let resposta = projetoController.criarProjeto(req);
-    if(resposta.startsWith('Erro')){
+    if(resposta.mensagem === 'erro'){
         //res.send('Erro ao cadastrar projeto');
-        res.redirect('/index');
+        res.redirect('/cadastra-projetoTeste');
     }else{
-        res.redirect('/meu-projeto');
+        let data = {
+            Mensagem: 'Projeto criado com sucesso!',
+            idProjeto:resposta.idProjeto,
+            Titulo:resposta.Titulo,
+            Publico:resposta.Publico,
+            Justificativa:resposta.Justificativaustificativa,
+            Status:resposta.Status,
+            Email:resposta.Email,
+            Objetivos: resposta.Objetivos
+        };
+        // Renderiza a página 'visualizacao-projeto.ejs' e envia para o cliente junto com os dados
+        res.render('visualizacao-projeto.ejs', data);
     }
 });
 
