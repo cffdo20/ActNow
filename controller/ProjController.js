f646b84013cf71a30cc00e2fc64cc86313f0904c
JDDPacheco <josediogo.dp@gmail.com>
2024-03-11 19:12:36 -0400

    versão para teste com validação de erro no controller

--- controller/ProjController.js
+++ controller/ProjController.js
@@ -1,15 +1,25 @@
 // Importando a classe ProjetoSocial
 const projeto = require('../model/ProjetoSocial.js');
+const dados = require('../model/ProjetoSocial.js');
 
 function criarProjeto(req) {
-    // Converter a Data para String
-    const data = new Date();
-    var dataInicioProj = data.toISOString(req.body.projDataInicio).split('T')[0];
-    console.log(dataInicioProj);
-
     /** Criação do novo projeto*/
-    return projeto.setProjetoSocial(req.body.projNome, req.body.projDescricao, req.body.projPublico,
+    projeto.setProjetoSocial(req.body.projNome, req.body.projDescricao, req.body.projPublico,
         req.body.projJustificativa, req.body.projObjetivos, dataInicioProj, '1', '1');
+
+    if (projeto.erro) {
+        return {
+            status: 'ERRO',
+            mensagem: projeto.erro,
+        }
+    } else {
+        dados.getProjetoSocial(req.body.projNome);
+        return {
+            status: 'SUCESSO',
+            mensagem: projeto.resposta,
+            dados: dados
+        }
+    }
 }
 
 module.exports = { criarProjeto };
