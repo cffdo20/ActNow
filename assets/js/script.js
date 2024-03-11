120b2861985bf1bcf951d008b784aa2b9ceb4b15
george-valente <valentegeorge71@gmail.com>
2024-03-10 20:55:57 -0400

    Para a Sprint 1

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/* Elementos */
+
+const btnTema = document.querySelector("#btn-tema"); 
+const body = document.body; 
+
+/* var typed = new Typed(".main-name", {
+    strings: ["Conheça o ActNow", "Meet ActNow"], 
+    typeSpeed: 150, 
+    backSpeed: 150, 
+    loop: true
+}) */
+
