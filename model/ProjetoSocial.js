ee80e70f45ce9e7d20936b38413dfa48298fc9eb
JDDPacheco <josediogo.dp@gmail.com>
2024-03-11 19:39:34 -0400

    Corrigido bug

--- model/ProjetoSocial.js
+++ model/ProjetoSocial.js
@@ -4,7 +4,7 @@ function setProjetoSocial(tituloProj, descricaoProj, publicoAlvoProj, justificat
   var parametros = [tituloProj, descricaoProj, publicoAlvoProj, justificativaProj, objetivosProj, dataInicioProj, '1', codUsuCriador];
   // Converter a Data para String
   const data = new Date();
-  var dataInicioProj = data.toISOString(req.body.projDataInicio).split('T')[0];
+  var dataInicioProj = data.toISOString(dataInicioProj).split('T')[0];
   console.log(dataInicioProj);
   // Retorna a promessa gerada pela função callProcedureWithParameter
   return bd.callProcedureWithParameter('sp_criar_projeto', parametros).then(consulta => {
