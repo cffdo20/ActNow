1ce670d6647a9c5d051805ffb1da1b2b446d74ca
JDDPacheco <josediogo.dp@gmail.com>
2024-03-11 19:11:39 -0400

    Versão de teste com configuração da data no model

--- model/ProjetoSocial.js
+++ model/ProjetoSocial.js
@@ -2,14 +2,21 @@ const bd = require('../BD/db.js');
 
 function setProjetoSocial(tituloProj, descricaoProj, publicoAlvoProj, justificativaProj, objetivosProj, dataInicioProj, codUsuCriador) {
   var parametros = [tituloProj, descricaoProj, publicoAlvoProj, justificativaProj, objetivosProj, dataInicioProj, '1', codUsuCriador];
+  // Converter a Data para String
+  const data = new Date();
+  var dataInicioProj = data.toISOString(req.body.projDataInicio).split('T')[0];
+  console.log(dataInicioProj);
   // Retorna a promessa gerada pela função callProcedureWithParameter
   return bd.callProcedureWithParameter('sp_criar_projeto', parametros).then(consulta => {
     return consulta[0][0];
   })
 }
 
-function getProjetoSocial(tituloProj){
+function getProjetoSocial(tituloProj) {
   // stub da função getProjetoSocial
-};
+  return bd.callProcedureWithParameter('sp_consultar_projeto', tituloProj).then(consulta => {
+    return consulta[0][0];
+  })
+}
 
-module.exports = { setProjetoSocial };
+module.exports = { setProjetoSocial, getProjetoSocial };
