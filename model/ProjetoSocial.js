5606d5b25f53c14224379a36c20b315ae21e86a6
JDDPacheco <josediogo.dp@gmail.com>
2024-03-11 22:19:28 -0400

    Mudanças feitas para testar em casa

--- model/ProjetoSocial.js
+++ model/ProjetoSocial.js
@@ -1,11 +1,32 @@
 const bd = require('../BD/db.js');
+const validar;
+
+function validarParametro(parametro, tipo) {
+  switch (tipo) {
+    case 'codigoUsuario':
+      if (parametro.isNumber()) {
+        return 'sucesso';
+      } else {
+        return 'erro';
+      }
+    default:
+      if (parametro === '') {
+        return 'erro';
+      } else {
+        return 'sucesso';
+      }
+  }
+
+}
 
 function setProjetoSocial(tituloProj, descricaoProj, publicoAlvoProj, justificativaProj, objetivosProj, dataInicioProj, codUsuCriador) {
   var parametros = [tituloProj, descricaoProj, publicoAlvoProj, justificativaProj, objetivosProj, dataInicioProj, '1', codUsuCriador];
+
   // Converter a Data para String
   const data = new Date();
   var dataInicioProj = data.toISOString(dataInicioProj).split('T')[0];
   console.log(dataInicioProj);
+
   // Retorna a promessa gerada pela função callProcedureWithParameter
   return bd.callProcedureWithParameter('sp_criar_projeto', parametros).then(consulta => {
     return consulta[0][0];
