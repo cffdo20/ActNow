4d5f7dc30930860f305033104a2a885f30f3ac51
cffdo <cffdo2.0@gmail.com>
2024-03-13 21:54:54 -0400

    Commitando arquivs criados

---
+++ Triggers.sql
@@ -0,0 +1,112 @@
+/*
+DELIMITER ##
+CREATE TRIGGER  AFTER UPDATE ON 
+FOR EACH ROW
+BEGIN
+    
+END; ##
+DELIMITER ;
+*/
+-- Triggers de LOG para projetosocial
+
+DELIMITER ##
+CREATE TRIGGER tg_log_insert_projetosocial AFTER INSERT ON projetosocial
+FOR EACH ROW
+BEGIN
+    
+END; ##
+DELIMITER ;
+
+DELIMITER ##
+CREATE TRIGGER tg_log_update_projetosocial AFTER UPDATE ON projetosocial
+FOR EACH ROW
+BEGIN
+    
+END; ##
+DELIMITER ;
+
+DELIMITER ##
+CREATE TRIGGER tg_log_delete_projetosocial AFTER DELETE ON projetosocial
+FOR EACH ROW
+BEGIN
+    
+END; ##
+DELIMITER ;
+
+-- Triggers de LOG para voluntário
+
+DELIMITER ##
+CREATE TRIGGER tg_log_insert_projetosocial AFTER INSERT ON projetosocial
+FOR EACH ROW
+BEGIN
+    
+END; ##
+DELIMITER ;
+
+DELIMITER ##
+CREATE TRIGGER tg_log_update_projetosocial AFTER UPDATE ON projetosocial
+FOR EACH ROW
+BEGIN
+    
+END; ##
+DELIMITER ;
+
+DELIMITER ##
+CREATE TRIGGER tg_log_delete_projetosocial AFTER DELETE ON projetosocial
+FOR EACH ROW
+BEGIN
+    
+END; ##
+DELIMITER ;
+
+-- Triggers de LOG para instituicao
+
+DELIMITER ##
+CREATE TRIGGER tg_log_insert_projetosocial AFTER INSERT ON projetosocial
+FOR EACH ROW
+BEGIN
+    
+END; ##
+DELIMITER ;
+
+DELIMITER ##
+CREATE TRIGGER tg_log_update_projetosocial AFTER UPDATE ON projetosocial
+FOR EACH ROW
+BEGIN
+    
+END; ##
+DELIMITER ;
+
+DELIMITER ##
+CREATE TRIGGER tg_log_delete_projetosocial AFTER DELETE ON projetosocial
+FOR EACH ROW
+BEGIN
+    
+END; ##
+DELIMITER ;
+
+-- Triggers de LOG para usuario
+
+DELIMITER ##
+CREATE TRIGGER tg_log_insert_projetosocial AFTER INSERT ON projetosocial
+FOR EACH ROW
+BEGIN
+    
+END; ##
+DELIMITER ;
+
+DELIMITER ##
+CREATE TRIGGER tg_log_update_projetosocial AFTER UPDATE ON projetosocial
+FOR EACH ROW
+BEGIN
+    
+END; ##
+DELIMITER ;
+
+DELIMITER ##
+CREATE TRIGGER tg_log_delete_projetosocial AFTER DELETE ON projetosocial
+FOR EACH ROW
+BEGIN
+    
+END; ##
+DELIMITER ;
