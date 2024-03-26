2622bda2befec2e4431c1b9b6bf04a44ab33ddf0
JDDPacheco <153141758+JDDPacheco@users.noreply.github.com>
2024-03-26 10:14:48 -0400

    Ajustados diretórios das páginas

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const bodyParser = require('body-parser');
 const app = express();
 // Configuração do EJS como view engine
 app.set('view engine', 'ejs');
-const diretorioDeViews = 'ActNow-front-end/view';
-const diretorioDoIndexHTML = 'ActNow-front-end';
+const diretorioDeViews = '/view';
+const diretorioDoIndexHTML = '/';
 // Definição do diretório onde estão os arquivos de visualização
 app.set('views', path.join(__dirname, diretorioDeViews));
 app.use(express.static(path.join(__dirname, diretorioDoIndexHTML)));
