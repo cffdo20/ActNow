68d318101f8a5556d2f4c1612ee85736f4c73377
JDDPacheco <153141758+JDDPacheco@users.noreply.github.com>
2024-03-26 11:36:58 -0400

    Diretório view adicionado

---
+++ view/assets/js/recrutar-voluntario.js
@@ -0,0 +1,13 @@
+const cleanFilterBtn = document.querySelector("#trashBtn"); 
+const checkboxes = document.querySelectorAll(".form-check-input"); 
+
+cleanFilterBtn.addEventListener("click", () => {
+    let inputs = checkboxes.length;
+
+    for(let i = 0; i < inputs; i++){
+        checkboxes[i].checked = false;
+    }
+
+})
+
+
---
+++ view/assets/js/script.js
@@ -0,0 +1,10 @@
+/* Elementos */
+
+const btnTema = document.querySelector("#btn-tema"); 
+const body = document.body; 
+const mainImg = document.querySelector("#index-main-img"); 
+
+document.addEventListener("DOMContentLoaded", function(){
+    mainImg.classList.remove("team-img")
+    mainImg.classList.add("fade-in")
+})
