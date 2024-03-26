89b1c75ccac3ca29b4a0c34fc5f12c3b54a1a439
JDDPacheco <153141758+JDDPacheco@users.noreply.github.com>
2024-03-26 15:32:18 -0400

    Importações e declarações de instâncias Express mais limpas

--- index.js
+++ index.js
@@ -2,24 +2,18 @@
 const projetoController = require('./controller/ProjetoController.js');
 const voluntarioController = require('./controller/VoluntarioController.js');
 
-// Importando 'ejs' para Engine View
-//const ejs = require('ejs');
-// Importando 'express' para requisiçõe HTTP e inicialização do host
-const path = require('path'); // Importando o módulo 'path'
+// Importando módulo Express
 const express = require('express');
-// Importando 'body-parser' para lidar com o corpo das requisições HTTP
-const bodyParser = require('body-parser');
-// Configurando const app para aplicar 'express'
 const app = express();
-// Configuração do EJS como view engine
+
+// Configura a view engine como EJS
 app.set('view engine', 'ejs');
-const diretorioDeViews = '/view/pages';
-const diretorioDoIndexHTML = '/view';
-// Definição do diretório onde estão os arquivos de visualização
-app.set('views', path.join(__dirname, diretorioDeViews));
-app.use(express.static(path.join(__dirname, diretorioDoIndexHTML)));
-// Configurando const app para 'body-parser' que lida com o conteúdo das requisições HTTP 
-app.use(bodyParser.urlencoded({ extended: false }));
+// Define o diretório onde estão os arquivos de visualização (views)
+app.set('views', 'view/pages');
+// Define o diretório onde estão os arquivos estáticos (como CSS, imagens, etc.)
+app.use(express.static('view'));
+// Middleware para fazer o parsing do corpo das requisições HTTP
+app.use(express.urlencoded({ extended: false }));
 
 /** Definição das Rotas */
 // Página: Index
@@ -27,33 +21,34 @@ app.get('/', (req,res) => {
     res.redirect('index.html');
 });
 
-// Página: Criação de novo projeto
+/** Página: Criação de novo projeto */
+// Acesso
+app.get('/projeto', (req, res) => {
+            res.render('cadastrar-projeto.ejs');
+});
+
+// Receber dados formulário e dar resposta
 app.post('/criar-projeto', (req, res) => {
     projetoController.criarProjeto(req)
     .then(resposta => {
         console.log(resposta);
         if(!resposta.erro){
-            // Se não retornar erro renderiza a pagina do projeto com a mensagem do banco e os dados do projeto
+            // Se não retornar erro renderiza a pagina do filtro com os dados de voluntários que atenderam aos critérios da busca
             res.render('visualizacao-projeto.ejs', resposta);
         }else{
-            // Redireciona de volta para página de cadastro de projeto enviando a mensagem de erro do banco como alert
+            // Em caso de erro renderiza a mesma página sem dados e enviando a mensagem de erro como alert
             res.render('cadastrar-projeto.ejs',{ alerta: resposta.erro });
         }
     });
 });
 
-// Página: Projeto
-app.get('/projeto', (req, res) => {
-            res.render('cadastrar-projeto.ejs');
-});
-
-
-// Página: Filtro de Voluntário
+/** Página: Filtro de Voluntário */
 // Acesso
 app.get('/recrutar-voluntario', (req, res) => {
     res.render('recrutar-voluntarios.ejs');
 });
-// Receber dados formulário
+
+// Receber dados formulário e dar resposta
 app.post('/recrutar-voluntario', (req, res) => {
     console.log(req.body);
     voluntarioController.filtrarVoluntario(req)
@@ -68,7 +63,6 @@ app.post('/recrutar-voluntario', (req, res) => {
         }
     });
 });
-
 
 /** Inicialização do servidor */
 // Setando a porta a ser usada no localhost
