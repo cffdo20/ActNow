c8890d6a026001312034f4d1efa10296f5ec971a
JDDPacheco <josediogo.dp@gmail.com>
2024-04-04 01:26:11 -0400

    Filtro de várias habilidades implementado no backend

--- model/Voluntario.js
+++ model/Voluntario.js
@@ -1,5 +1,12 @@
 const bd = require('./db.js');
 
+function eliminarItensRepetidos(array) {
+  // Cria um novo Set a partir do array para eliminar itens duplicados
+  var conjunto = new Set(array);
+  // Retorna um novo array convertendo o Set de volta para um array
+  return Array.from(conjunto);
+}
+
 function getFiltroVoluntario(filtroDiaSemana, voluntarioHorario, voluntarioHabilidades) {
   // Tratar horário
   var matutino = '0' , vespertino = '0', noturno = '0';
@@ -31,13 +38,46 @@ function getFiltroVoluntario(filtroDiaSemana, voluntarioHorario, voluntarioHabil
     }
   }
 
-  var parametros = [filtroDiaSemana, matutino, vespertino, noturno, voluntarioHabilidades];
   console.log(parametros);
-  // Retorna a promessa gerada pela função callProcedureWithParameter
-  return bd.callProcedureWithParameter('sp_filtrar_voluntarios', parametros).then(consulta => {
-    console.log(consulta);
-    return consulta[0][0];
-  })
+  if(Array.isArray(voluntarioHabilidades)){
+    var parametrosBase = [filtroDiaSemana, matutino, vespertino, noturno];
+    var respostasUnicas = new Set(); // Conjunto para armazenar respostas únicas
+
+    // Mapeia cada habilidade do voluntário para uma promessa de consulta
+    var promessasConsulta = voluntarioHabilidades.map(habilidade => {
+        // Cria uma cópia dos parâmetros base e adiciona a habilidade atual
+        var parametros = parametrosBase.slice(); // Copia os parâmetros base
+        parametros.push(habilidade); // Adiciona a habilidade atual
+
+        // Retorna a promessa gerada pela função callProcedureWithParameter
+        return bd.callProcedureWithParameter('sp_filtrar_voluntarios', parametros).then(consulta => {
+            var resposta = consulta[0][0];
+            // Verifica se a resposta não está vazia e se não está presente no conjunto de respostas únicas
+            if (resposta && !respostasUnicas.has(JSON.stringify(resposta))) {
+                respostasUnicas.add(JSON.stringify(resposta)); // Adiciona a resposta ao conjunto de respostas únicas
+                return resposta;
+            } else {
+                // Se a resposta estiver vazia ou repetida, retorna undefined para que não seja adicionada ao array de respostas
+                return undefined;
+            }
+        });
+    });
+
+    // Retorna uma promessa que será resolvida quando todas as consultas forem concluídas
+    return Promise.all(promessasConsulta).then(respostas => {
+        // Filtra as respostas para remover os elementos undefined
+        var respostasValidas = respostas.filter(resposta => resposta !== undefined);
+        // Retorna as respostas válidas como um array
+        return respostasValidas;
+    });
+  }else{
+    var parametros = [filtroDiaSemana, matutino, vespertino, noturno, voluntarioHabilidades];
+    // Retorna a promessa gerada pela função callProcedureWithParameter
+    return bd.callProcedureWithParameter('sp_filtrar_voluntarios', parametros).then(consulta => {
+      console.log(consulta);
+      return consulta[0][0];
+    })
+  }
 }
 
 function getVoluntario(userName){
