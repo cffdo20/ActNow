82d19f5f6372823d48c12aeda8e869c864e52542
george-valente <valentegeorge71@gmail.com>
2024-04-13 16:03:24 -0400

    Adicionado interação ao Modal

--- view/assets/js/visualizar-projeto.js
+++ view/assets/js/visualizar-projeto.js
@@ -2,9 +2,12 @@ const addAtvButton = document.querySelector("#addAtividadeButton");
 const atvText = document.querySelector(".add-atv-button"); 
 const tooltip = document.querySelector("#tooltip"); 
 
+const trashBtn = document.querySelector("#trashBtn")
+const modal = document.querySelector("dialog"); 
+const returnBtn = document.querySelector("#returnPopBtn"); 
 
 
-addAtvButton.addEventListener("mouseover", () => {
+/* addAtvButton.addEventListener("mouseover", () => {
     tooltip.style.display = "block"; 
 
 })
@@ -12,4 +15,13 @@ addAtvButton.addEventListener("mouseout", () => {
     tooltip.style.display = "none"; 
 
 })
+ */
+trashBtn.addEventListener("click", (e) => {
+    e.preventDefault();
 
+    modal.showModal(); 
+})
+
+returnBtn.addEventListener("click", () => {
+    modal.close(); 
+})
