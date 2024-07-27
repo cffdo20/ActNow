f0f61789a018cec04fa7e007de8a19b1c0c8f05a
JDDPacheco <josediogo.dp@gmail.com>
2024-07-26 23:50:11 -0400

    editar de projeto

--- routes/ProjetoRoutes.js
+++ routes/ProjetoRoutes.js
@@ -23,4 +23,25 @@ router.post('/criar-projeto', (req, res) => {
     });
 });
 
+/** Página: Editar o projeto */
+// Acesso
+router.get('/editar-projeto', (req, res) => {
+    res.render('editar-projeto.ejs', req);
+});
+
+// Receber dados formulário e dar resposta
+router.post('/projeto-editado', (req, res) => {
+    projetoController.editarProjeto(req)
+    .then(resposta => {
+        console.log(resposta);
+        if(!resposta.erro){
+            // Se não retornar erro renderiza a pagina do projeto com os dados editados
+            res.render('visualizacao-projeto.ejs', resposta);
+        }else{
+            // Em caso de erro renderiza a mesma página sem dados e enviando a mensagem de erro como alert
+            res.render('editar-projeto.ejs',{req, alerta: resposta.erro });
+        }
+    });
+});
+
 module.exports = router;
