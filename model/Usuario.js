9947251758b0cdeaf885df2a4a633d2cf2413698
JDDPacheco <josediogo.dp@gmail.com>
2024-08-02 01:14:37 -0400

    Criado model de Usuario comm getter e setter e adicionada página de cadastro de usuário

--- model/Usuario.js
+++ model/Usuario.js
@@ -1,21 +1,17 @@
 const bd = require('./db.js');
 
-function setUsuario(tituloProj, descricaoProj, publicoAlvoProj, justificativaProj, objetivosProj, dataInicioProj, codUsuCriador) {
-    // Converter a Data para String
-    const data = new Date();
-    const dataInicio = data.toISOString(dataInicioProj).split('T')[0];
-    console.log(dataInicio);
-    let parametros = [tituloProj, descricaoProj, publicoAlvoProj, justificativaProj, objetivosProj, dataInicio, '1', codUsuCriador];
+function setUsuario(userName, userSenha, userEmail) {
+    let parametros = [userName, userSenha, userEmail];
     // Retorna a promessa gerada pela função callProcedureWithParameter
-    return bd.callProcedureWithParameter('sp_criar_projeto', parametros).then(consulta => {
+    return bd.callProcedureWithParameter('sp_criar_usuario', parametros).then(consulta => {
       return consulta[0][0];
     })
   }
   
-  function getUsuario(tituloProj){
-    let parametros = [tituloProj];
+  function getUsuario(userName){
+    let parametros = [userName];
     // Retorna a promessa gerada pela função callProcedureWithParameter
-    return bd.callProcedureWithParameter('sp_consultar_projeto', parametros).then(consulta => {
+    return bd.callProcedureWithParameter('sp_consultar_usuario', parametros).then(consulta => {
       return consulta[0][0];
     })
   };
