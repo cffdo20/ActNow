655f94907647cf69e5c7c542897cbebd9289d149
george-valente <valentegeorge71@gmail.com>
2024-08-03 16:55:14 -0400

    changing text index

--- view/assets/js/script.js
+++ view/assets/js/script.js
@@ -3,6 +3,7 @@
 const btnTema = document.querySelector("#btn-tema"); 
 const body = document.body; 
 const mainImg = document.querySelector("#index-main-img"); 
+const textToChange = document.querySelector(".text-to-change"); 
 
 
 const addAtvButton = document.querySelector("#addAtividadeButton");
@@ -13,5 +14,23 @@ document.addEventListener("DOMContentLoaded", function(){
     mainImg.classList.remove("team-img")
     mainImg.classList.add("fade-in")
 })
+
+const textLoad = () => {
+    setTimeout(() => {
+        textToChange.textContent = "Aja agora!"
+        
+    }, 0);
+    setTimeout(() => {
+        textToChange.textContent = "Act Now!"
+        
+    }, 4000);
+    setTimeout(() => {
+        textToChange.textContent = "ActNow"
+        
+    }, 8000);
+}
+
+textLoad(); 
+setInterval(textLoad, 12000)
 
 
