5b3310e84a373c5039e33597c36906f1cbdd6a7a
jddpacheco <josediogo.dp@gmail.com>
2024-08-07 18:38:32 -0400

    Criados arquivos de rotas de  sessão

--- routes/SessionRoutes.js
+++ routes/SessionRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const sessionController = require('../controller/SessionController.js');
 
-// Direcionar para Formulário de Criação de Usuario
+// Direcionar para Formulário de Login
 router.get('/login', (req, res) => {
   res.render('login.ejs');
 });
