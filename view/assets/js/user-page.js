370c31768b9a47c3c65f7c07dd2ab5e886f4a93a
george <valentegeorge71@gmail.com>
2024-08-22 18:01:57 -0400

    ajustes nos scripts

---
+++ view/assets/js/user-page.js
@@ -0,0 +1,6 @@
+const mainImg = document.querySelector("#homepage-main-img"); 
+
+document.addEventListener("DOMContentLoaded", function(){
+    mainImg.classList.remove("team-img")
+    mainImg.classList.add("fade-in")
+})
