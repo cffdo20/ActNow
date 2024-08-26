d7bc18595c2f0b4b484d1acc0979fb00d8e3ee11
george <valentegeorge71@gmail.com>
2024-08-25 22:05:08 -0400

    modais para edição de informações de voluntário

--- view/assets/js/user-profile.js
+++ view/assets/js/user-profile.js
@@ -12,6 +12,23 @@ const openEditPasswordModal = document.querySelector("#edit-password-button");
 const editPasswordModal = document.querySelector("#editSenhaModal");
 const closeEditPasswordModal = document.querySelector("#close-edit-password-dialog");
 
+/* trocar bio */
+const openEditBioModal = document.querySelector(".volunteer-bio");
+const editBioModal = document.querySelector("#editBioModal"); 
+const closeEditBioModal = document.querySelector("#close-edit-bio-dialog");
+
+/* trocar telefone */
+const openEditPhoneModal = document.querySelector(".volunteer-phone");
+const editPhoneModal = document.querySelector("#editPhoneModal");
+const closeEditPhoneModal = document.querySelector("#close-edit-phone-dialog");
+
+/* trocar cidade */
+const openEditCityModal = document.querySelector(".volunteer-location");
+const editCityModal = document.querySelector("#editCityModal");
+const closeEditCityModal = document.querySelector("#close-edit-city-dialog");
+
+
+
 openDeleteProfileModal.addEventListener("click", (e) => {
      e.preventDefault();
     deleteProfileModal.showModal();
@@ -38,3 +55,63 @@ closeEditPasswordModal.addEventListener("click", () => {
     editPasswordModal.close(); 
 })
 
+/* trocar bio */
+openEditBioModal.addEventListener("click", () => {
+    editBioModal.showModal();
+})
+
+closeEditBioModal.addEventListener("click", () => {
+    editBioModal.close();
+})
+
+/* trocar telefone */
+openEditPhoneModal.addEventListener("click", () => {
+    editPhoneModal.showModal();
+})
+
+closeEditPhoneModal.addEventListener("click", () => {
+    editPhoneModal.close(); 
+})
+
+/* trocar cidade */
+openEditCityModal.addEventListener("click", () =>{
+    editCityModal.showModal();
+})
+
+closeEditCityModal.addEventListener("click", () => {
+    editCityModal.close();
+})
+
+
+/* lista dinâmica de cidades */
+document.addEventListener("DOMContentLoaded", () => {
+    const estadoSelect = document.querySelector('select[name="volEstado"]');
+    const cidadeSelect = document.querySelector('select[name="volCidNome"]');
+
+    estadoSelect.addEventListener('change', () => {
+        const estado = estadoSelect.value; // Corrigido
+
+        if (estado) {
+            fetch(`/voluntarios/cidades/${estado}`) 
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Erro de conexão');
+                    }
+                    return response.json();
+                })
+                .then(cidades => {
+                    cidadeSelect.innerHTML = '<option selected>Cidade</option>';
+                    cidades.forEach(cidade => {
+                        const option = document.createElement('option');
+                        option.value = cidade.cidnome; 
+                        option.textContent = cidade.cidnome;
+
+                        cidadeSelect.appendChild(option);
+                    });
+                })
+                .catch(error => console.log('Erro ao buscar cidades', error));
+        } else {
+            cidadeSelect.innerHTML = '<option selected>Cidade</option>';
+        }
+    });
+});
