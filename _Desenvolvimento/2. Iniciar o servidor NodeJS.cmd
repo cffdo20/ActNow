e1d09d145584a0695801a41edf4e9e43be62c9d1
JDDPacheco <153141758+JDDPacheco@users.noreply.github.com>
2024-08-27 20:15:34 -0400

    Delete _Desenvolvimento/2. Iniciar o servidor NodeJS.cmd
    Arquivo não necessário

--- _Desenvolvimento/2. Iniciar o servidor NodeJS.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-:: Inicia o Script Node.js
-start cmd /k "cd D:\Salve_Aqui_Seus_Arquivos\DIOGO\ActNow && node index.js"
-
-:: Abre o Microsoft Edge com a URL
-start msedge "http://localhost:3000/"
